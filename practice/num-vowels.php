c404449f9f025e4631318a5789b50e4f038d9b81
Kristen Gondim <kristengondim@gmail.com>
2022-10-17 19:38:37 +0000

    Week 7 practice

--- practice/num-vowels.php
+++ practice/num-vowels.php
@@ -1,26 +1,8 @@
-<!-- prompts user to guess a random number that computer generates -->
-
 <?php
-
-// function checkNumber($guess, $mysteryNumber)
-// {
-//     if($guess == $mysteryNumber) {
-//       return 'correct';
-//     } else if($guess > $mysteryNumber) {
-//         return 'high';
-//     } else {
-//         return 'low';
-//     }
-// }
-
-// var_dump(checkNumber(6, 3));
-// var_dump(checkNumber(3, 7));
-// var_dump(checkNumber(3, 3));
-
 
 # for loop code influenced by: https://t4tutorials.com/php-program-to-count-the-vowels-digits-and-spaces-in-a-string/#:~:text=%2Fhtml%3E%20%3C%3F-,php%20if(%24_POST)%20%7B%20%24string%20%3D%20strtolower(%24_POST,%2C%20'%20')%3B%20echo%20%22
 
-$word = "kristen";
+$word = "heeet";
 function vowelCount($word)
 {
     
@@ -33,7 +15,7 @@ function vowelCount($word)
             $num++;
         }
     }
-    echo "Number of vowels:". $num;
+    echo "Number of vowels: " . $num;
 }
 
 vowelCount($word);
