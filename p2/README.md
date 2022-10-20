352d8d9de1e738a436de9f5f0288229d39125829
Kristen Gondim <kristengondim@gmail.com>
2022-10-20 21:47:18 +0000

    Project 2 setup

---
+++ p2/README.md
@@ -0,0 +1,11 @@
+*Any instructions/notes in italics should be removed from the template before submitting* 
+
+# Project 2
++ By: Kristen
++ URL: <http://e2p2.vestige-kg.me>
+
+## Outside resources
+*your list of outside resources go here*
+
+## Notes for instructor
+*any notes for me to refer to while grading; if none, omit this section*
---
+++ p2/index-view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>Project 2 - Dice</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+
+</head>
+
+<body>
+
+    <h1>Project 2 - Dice</h1>
+
+    <form method='POST' action='process.php'>
+
+        <input type='radio' id='heads' name='' value=''><label for='heads'></label>
+        <input type='radio' id='tails' name='' value=''><label for='tails'></label>
+
+        <button type='submit'>Play</button>
+
+
+</body>
+
+</html>
