02e1ae49f254f979912f8b75612f0abdf666d795
Kristen Gondim <kristengondim@gmail.com>
2022-11-05 18:05:25 +0000

    week 10 practice

--- zipfoods/app/Controllers/AppController.php
+++ zipfoods/app/Controllers/AppController.php
@@ -21,4 +21,12 @@ class AppController extends Controller
             'email' => 'support@zipfoods.com'
         ]);
     }
+
+    public function about()
+    {
+        return $this->app->view('about', [
+            'aboutus' => 'ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.'
+        ]);
+    }
+    
 } 
